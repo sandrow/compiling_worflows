ce55be7e368a0ad0a58e2fc934a2e7655b9cab37
Sandrow <sandrow.townsend@hotmail.com>
2023-08-09 19:22:44 -0400

    script to compile workflows for deployment

---
+++ compiling_worflows.js
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////////////////////////////////////
+//Automating workflow consolidation in AEM for workflow deployment (JQuery)
+//This is to be ran as a browser snippet while on the workflow page in AEM
+
+$( 'a' ).each(function( index ) { 
+
+    console.log( $( this ).text() ); 
+  
+});
